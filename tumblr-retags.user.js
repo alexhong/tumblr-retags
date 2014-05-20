fe67aea9dcd1508c568e72054b68574bd71d9a85
Alex Hong <alex@alexhong.net>
2014-05-20 11:53:22 -0700

    0.4
    fall back to standard request if GM_xmlhttpRequest not available (e.g.
    if Chrome extension)

--- tumblr-retags.user.js
+++ tumblr-retags.user.js
@@ -2,8 +2,9 @@
 // @name        Tumblr Retags
 // @namespace   http://alexhong.net/
 // @description Adds tags to reblog notes, and wraps all tags for readability.
-// @version     0.3.1
+// @version     0.4
 // @include     *://www.tumblr.com/*
+// @downloadURL https://github.com/alexhong/tumblr-retags/raw/master/tumblr-retags.user.js
 // ==/UserScript==
 
 var retags = {
@@ -16,80 +17,103 @@ var retags = {
 		});
 	}),
 	run: function(){
+		retags.observer.observe(document,{childList:true,subtree:true});
 		$('head').append(retags.css);
 		retags.tag($(retags.selectors));
-		retags.observer.observe(document,{childList:true,subtree:true});
 		$(document).on('mouseover','.post_tags_inner',function(){
-			$(this).removeClass('post_tags_inner').addClass('DISABLED_post_tags_inner');
+			$(this).attr('class','DISABLED_post_tags_inner');
 		});
 	},
 	destroy: function(){
-		$('#retags-css,.retags').remove();
 		retags.observer.disconnect();
-		$('.DISABLED_post_tags_inner').removeClass('DISABLED_post_tags_inner').addClass('post_tags_inner');
+		$('.retags-css,.retags').remove();
+		$('.DISABLED_post_tags_inner').attr('class','post_tags_inner');
 	},
 	tag: function($el){
 		$el.each(function(){
 			if ($(this).find('.retags').length)
-				return false;
-			var container_class = '';
-			if ($(this).hasClass('notification')) {
-				var $container = $(this).find('.notification_sentence');
-				var url = $container.find('.notification_target').attr('href');
+				return;
+			var url = '';
+			// popover
+			if ($(this).hasClass('note')) {
+				var $container = $(this);
+				var url = $container.find('.action').data('post-url');
+			// Activity
 			} else if ($(this).hasClass('ui_note')) {
 				var $container = $(this).find('.stage');
 				var url = $container.find('.part_glass').attr('href');
-			} else if ($(this).hasClass('note')) {
-				var $container = $(this);
-				var url = $container.find('.action').data('post-url');
-				var container_class = 'with_commentary';
+			// dashboard
+			} else if ($(this).hasClass('notification')) {
+				var $container = $(this).find('.notification_sentence');
+				var url = $container.find('.notification_target').attr('href');
 			}
-			if (url) {
-				var $retags = $('<div class="retags">');
-				GM_xmlhttpRequest({
-					method: 'GET',
-					url: url.replace(/^http:\/\/(.+)\/post\/(\d+).*/g,retags.api_url+retags.api_key),
-					onload: function(data){
-						var tags = JSON.parse(data.responseText).response.posts[0].tags;
-						if (tags.length) {
-							tags.forEach(function(tag){
-								$retags.append('<a href="//tumblr.com/tagged/'+tag.replace(/ /g,'-')+'">#'+tag+'</a>');
-							});
-							$container.addClass(container_class).append($retags);
-						}
-					},
-					onerror: function(){
-						$retags.addClass('error').append('Could not access post.');
-						$container.addClass(container_class).append($retags);
-					}
-				});
-			}
+			if (url)
+				retags.request($container,url.replace(/^http:\/\/(.+)\/post\/(\d+).*/g,retags.api_url+retags.api_key));
 		});
 	},
-	css: '<style id="retags-css">\
-			.retags { white-space: normal; margin-top: 10px; }\
-			.retags.error { color: #c00000; }\
-			.retags a { color: #a7a7a7 !important; position: relative; margin-right: 11px; text-decoration: none; }\
-			.retags a:hover { color: #969696 !important; }\
-			.note .retags { font-size: 12px; line-height: 1.3; }\
-			.note .retags a { margin-right: 9px; }\
-			.post_notes .notes_outer_container.popover .note.with_commentary span.action { min-height: 0; }\
-			.notification .retags a { color: rgba(255,255,255,0.3) !important; }\
-			.notification .retags a:hover { color: rgba(255,255,255,0.4) !important; }\
-			.ui_note .retags { margin-top: 0; padding: 40px 50px 13px; }\
-			.ui_note .part_response + .retags { margin-top: -7px; padding-top: 0; }\
-			.post_full .post_tags { white-space: normal; padding: 1px 0; line-height: 18px; }\
-			.post_full .post_tags:after { display: none; }\
-			.post_full .post_tags .post_tag,\
-			.post_full .post_tags .post_tag.featured { display: inline; padding-top: 2px; padding-bottom: 2px; }\
-			.post_full .post_tags .post_tag:after,\
-			.retags a:after { content: "\\00a0  "; font-size: 0; line-height: 0; }\
-		</style>'
+	request: 
+		(typeof GM_xmlhttpRequest !== 'undefined')
+		// if userscript or XKit extension
+		? function($container,url) {
+			GM_xmlhttpRequest({
+				method: 'GET',
+				url: url,
+				onload: function(data){
+					retags.append($container,JSON.parse(data.responseText).response.posts[0].tags);
+				},
+				onerror: function(){
+					retags.append($container,'error');
+				}
+			});
+		}
+		// if Chrome extension
+		: function($container,url) {
+			var tags;
+			$.getJSON(url,function(data){
+				retags.append($container,data.response.posts[0].tags);
+			}).fail(function(){
+				retags.append($container,'error');
+			});
+		}
+	,
+	append: function($container,tags){
+		var $retags = $('<div class="retags">');
+		var container_class = $container.hasClass('note') ? 'with_commentary' : '';
+		if (tags === 'error') {
+			$retags.addClass('error').append('Could not access post.');
+			$container.addClass(container_class).append($retags);					
+		} else if (tags.length) {
+			tags.forEach(function(tag){
+				$retags.append('<a href="//tumblr.com/tagged/'+tag.replace(/ /g,'-')+'">#'+tag+'</a>');
+			});
+			$container.addClass(container_class).append($retags);
+		}
+	},
+	css: 
+	'<style class="retags-css">\
+		.retags { white-space: normal; margin-top: 10px; }\
+		.retags.error { color: #c00000; }\
+		.retags a { color: #a7a7a7 !important; position: relative; margin-right: 11px; text-decoration: none; }\
+		.retags a:hover { color: #969696 !important; }\
+		.note .retags { font-size: 12px; line-height: 1.3; }\
+		.note .retags a { margin-right: 9px; }\
+		.post_notes .notes_outer_container.popover .note.with_commentary span.action { min-height: 0; }\
+		.notification .retags a { color: rgba(255,255,255,0.3) !important; }\
+		.notification .retags a:hover { color: rgba(255,255,255,0.4) !important; }\
+		.ui_note .retags { margin-top: 0; padding: 40px 50px 13px; }\
+		.ui_note .part_response + .retags { margin-top: -7px; padding-top: 0; }\
+		.post_full .post_tags { white-space: normal; padding: 1px 0; line-height: 18px; }\
+		.post_full .post_tags:after { display: none; }\
+		.post_full .post_tags .post_tag,\
+		.post_full .post_tags .post_tag.featured { display: inline; padding-top: 2px; padding-bottom: 2px; }\
+		.post_full .post_tags .post_tag:after,\
+		.retags a:after { content: "\\00a0  "; font-size: 0; line-height: 0; }\
+	</style>'
 };
 
 if (typeof XKit !== 'undefined') {
 	//* TITLE Retags **//
-	//* VERSION 0.3.1 **//
+	//* VERSION 0.4 **//
 	//* DESCRIPTION Adds tags to reblog notes, and wraps all tags for readability. **//
 	//* DEVELOPER alexhong **//
 	//* FRAME false **//
